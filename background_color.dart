6face5887b9b4961a3c3bc84baa90add7233f1cd
CodingWithZohaib <113612574+Zohaib521321@users.noreply.github.com>
2023-06-09 23:12:08 -0700

    Add files via upload

---
+++ background_color.dart
@@ -0,0 +1,52 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("LinearProgressIndicator"),
+        ),
+        body:
+        Center(
+          child: LinearProgressIndicator(
+            value: 0.7,
+            color: Colors.red,
+
+            semanticsLabel: 'Linear progress indicator',
+          ),
+        ),
+    );
+  }
+
+}
